221cb5dbf98b9afa014e42f79aaae4aa43eaaa70
NureZakharovAnton <anton.zakharov1@nure.ua>
2024-12-06 18:06:10 +0200

    init: create nest project

---
+++ Task2/arkpz-pzpi-22-2-zakharov-anton-task2/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
